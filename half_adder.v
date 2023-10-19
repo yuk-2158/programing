f648452bd6a2a1a6f56c4c8c03bda4fa7223275b
yuka <52622723+yuk-2158@users.noreply.github.com>
2023-10-19 21:22:45 +0900

    Create half_adder.v

---
+++ half_adder.v
@@ -0,0 +1,11 @@
+timescale 1ns/1ps
+// Module Name: half_adder
+
+module half_adder(
+  input wire x1,
+  input wire x0,
+  output wire [1:0] y
+);
+  assign y[1] = x1 & x0;
+  assign y[0] = (!(x1&x0)) & (x1|x0);
+endmodule
